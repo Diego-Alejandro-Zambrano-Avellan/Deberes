09d8f5a1e5c70276e526297d80e746a4dd433ab6
Diego-Alejandro-Zambrano-Avellan <zambranoavellan@gmail.com>
2024-06-07 08:08:41 -0500

    primero

---
+++ script.js
@@ -0,0 +1,9 @@
+function addTask() {
+   
+    const title = document.getElementById('title').value;
+    const description = document.getElementById('description').value;
+    const dueDate = document.getElementById('dueDate').value;
+    const priority = document.getElementById('priority').value;
+    
+
+}
